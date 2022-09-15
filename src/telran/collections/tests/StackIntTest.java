d6e797528a12ffc90b886af6342fc6eaf69529f0
Yuragranovsky <yuragranovsky@gmail.com>
2022-09-15 21:20:17 +0300

    adds CW #27 with TODO's for HW #27

---
+++ src/telran/collections/tests/StackIntTest.java
@@ -0,0 +1,29 @@
+package telran.collections.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StackIntTest {
+
+	@Test
+	void testPop() {
+		//TODO
+	}
+
+	@Test
+	void testPush() {
+		//TODO
+	}
+
+	@Test
+	void testIsEmpty() {
+		//TODO
+	}
+
+	@Test
+	void testGetMaxNumber() {
+		//TODO
+	}
+
+}
---
+++ src/telran/util/StackInt.java
@@ -0,0 +1,27 @@
+package telran.util;
+
+public class StackInt {
+	//TODO fields
+	//complexity of all following methods is O[1]
+public int pop() {
+	//TODO
+	//removes last number and returns removed number
+	//throws exception NoSuchElementException for empty stack
+	return 0;
+}
+public void push(int number) {
+	//TODO
+	//adds number at end of a stack
+}
+public boolean isEmpty() {
+	//TODO
+	//returns true if a stack is empty
+	return false;
+}
+public int getMaxNumber() {
+	//TODO
+	//returns maximal number existing in a stack
+	//throws exception NoSuchElementException for empty stack
+	return 0;
+}
+}
