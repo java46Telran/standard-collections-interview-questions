d2e24c56fa88c9c4c6ab64214ec4da5502f8bb4c
Yuragranovsky <yuragranovsky@gmail.com>
2022-09-19 18:04:09 +0300

    adds HW #27 StackInt with complexity O[1]

--- src/telran/util/StackInt.java
+++ src/telran/util/StackInt.java
@@ -1,27 +1,40 @@
 package telran.util;
 
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
 public class StackInt {
-	//TODO fields
-	//complexity of all following methods is O[1]
+	private LinkedList<Integer> numbers = new LinkedList<>();
+	private LinkedList<Integer> maxNumbers = new LinkedList<>();
 public int pop() {
-	//TODO
 	//removes last number and returns removed number
 	//throws exception NoSuchElementException for empty stack
-	return 0;
+	if (numbers.isEmpty()) {
+		throw new NoSuchElementException();
+	}
+	int res = numbers.removeLast();
+	if (res == maxNumbers.getLast()) {
+		maxNumbers.removeLast();
+	}
+	return res;
 }
 public void push(int number) {
-	//TODO
 	//adds number at end of a stack
+	numbers.add(number);
+	if (maxNumbers.isEmpty() || number >= maxNumbers.getLast()) {
+		maxNumbers.add(number);
+	}
 }
 public boolean isEmpty() {
-	//TODO
 	//returns true if a stack is empty
-	return false;
+	return numbers.isEmpty();
 }
 public int getMaxNumber() {
-	//TODO
 	//returns maximal number existing in a stack
 	//throws exception NoSuchElementException for empty stack
-	return 0;
+	if (maxNumbers.isEmpty()) {
+		throw new NoSuchElementException();
+	}
+	return maxNumbers.getLast();
 }
 }
