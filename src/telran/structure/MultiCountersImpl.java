1949216900a5b93cc08d896801ab6fdfc0dc2fa6
Yuragranovsky <yuragranovsky@gmail.com>
2022-10-06 15:26:37 +0300

    HW #31 MultiCountersImpl & Tests

--- src/telran/structure/MultiCountersImpl.java
+++ src/telran/structure/MultiCountersImpl.java
@@ -1,33 +1,52 @@
 package telran.structure;
 
 import java.util.*;
+import java.util.Map.Entry;
 
 public class MultiCountersImpl implements MultiCounters {
- HashMap<Object, Integer> items = new HashMap<>(); //key - item, value - counter
- TreeMap<Integer, HashSet<Object>> counters = new TreeMap<>(); //key counter, value - set of items having the key - counter
+	HashMap<Object, Integer> items = new HashMap<>(); // key - item, counter - counter
+	TreeMap<Integer, HashSet<Object>> counters = new TreeMap<>(); // key counter, counter - set of items having the key
+																	// -
+																	// counter
 //consider using the Map method computeIfAbsent	
- @Override
+
+	@Override
 	public Integer addItem(Object item) {
-		// TODO Auto-generated method stub
-		return null;
+		Integer res = items.merge(item, 1, Integer::sum);
+		if (res > 1) {
+			counterItemsRemove(res - 1, item);
+		}
+		counters.computeIfAbsent(res, e -> new HashSet<>()).add(item);
+		return res;
+	}
+
+	private void counterItemsRemove(int counter, Object item) {
+		HashSet<Object> set = counters.get(counter);
+		set.remove(item);
+		if (set.isEmpty()) {
+			counters.remove(counter);
+		}
 	}
 
 	@Override
 	public Integer getValue(Object item) {
-		// TODO Auto-generated method stub
-		return null;
+
+		return items.get(item);
 	}
 
 	@Override
 	public boolean remove(Object item) {
-		// TODO Auto-generated method stub
-		return false;
+		Integer counter = items.remove(item);
+		if (counter != null) {
+			counterItemsRemove(counter, item);
+		}
+		return counter != null;
 	}
 
 	@Override
 	public Set<Object> getMaxItems() {
-		// TODO Auto-generated method stub
-		return null;
+		Entry<Integer, HashSet<Object>> maxCounter = counters.lastEntry() ;
+		return maxCounter != null ? maxCounter.getValue() : Collections.emptySet();
 	}
 
 }
---
+++ src/telran/structure/MultiCountersTest.java
@@ -0,0 +1,68 @@
+package telran.structure;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MultiCountersTest {
+MultiCounters multiCounters;
+@BeforeEach
+void setUp() {
+	multiCounters = new MultiCountersImpl();
+	multiCounters.addItem("a");
+	multiCounters.addItem("a");
+	multiCounters.addItem("a");
+	multiCounters.addItem(123);
+	multiCounters.addItem(123);
+	multiCounters.addItem("b");
+	
+}
+	@Test
+	void addItemTest() {
+		assertEquals(4, multiCounters.addItem("a"));
+		assertEquals(1, multiCounters.addItem("abcd12"));
+		assertEquals(1, multiCounters.addItem(LocalDate.now()));
+		assertEquals(2, multiCounters.addItem(LocalDate.now()));
+		
+	}
+	@Test
+	void getValueTest() {
+		assertEquals(3, multiCounters.getValue("a"));
+		assertEquals(2, multiCounters.getValue(123));
+		assertEquals(1, multiCounters.getValue("b"));
+		assertNull(multiCounters.getValue("c"));
+	}
+	@Test
+	void removeTest() {
+		assertTrue(multiCounters.remove(123));
+		assertFalse(multiCounters.remove(123));
+	}
+	@Test
+	void getMaxItemsTest() {
+		Object expected[] = {"a"};
+		runArraySet(expected, multiCounters.getMaxItems());
+		multiCounters.addItem(123);
+		Object expected1[] = {"a", 123};
+		runArraySet(expected1, multiCounters.getMaxItems());
+		multiCounters.remove(123);
+		runArraySet(expected, multiCounters.getMaxItems());
+		multiCounters.remove("a");
+		Object expected2[] = {"b"};
+		runArraySet(expected2, multiCounters.getMaxItems());
+		multiCounters.remove("b");
+		runArraySet(new Object[0], multiCounters.getMaxItems());
+		
+	}
+	private void runArraySet(Object[] expected, Set<Object> maxItems) {
+		assertEquals(expected.length, maxItems.size());
+		for(Object item: expected) {
+			assertTrue(maxItems.contains(item));
+		}
+		
+	}
+
+}
